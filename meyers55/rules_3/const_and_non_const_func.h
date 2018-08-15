ca84bbe4ecd8307b55e281f2d9de294dc954d435
sonulen <sonulen@gmail.com>
2018-08-15 17:57:33 +0300

    update: В процессе изучения const

--- meyers55/rules_3/const_and_non_const_func.h
+++ meyers55/rules_3/const_and_non_const_func.h
@@ -5,6 +5,19 @@
 
 class rules3 {
 public:
+	void sing_song () {
+		std::cout << "Kiki, do you love me? Are you riding? \
+					Say you'll never ever leave from beside me" << std::endl;
+	}
+
+	void roar () const {
+		std::cout << "const roar" << std::endl;
+	}
+
+	void roar () {
+		std::cout << "non const roar" << std::endl;
+	}
+
 	const int& my_num () const {
 		std::cout << "const" << std::endl;
 		return this->id;
