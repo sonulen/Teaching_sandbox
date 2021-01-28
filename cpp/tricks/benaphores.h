93645bcf8d0b89577f647101c535e92b9237cb57
Andrey Tolmachev <sonulen@gmail.com>
2021-01-28 22:10:01 +0300

    Create benaphores.h

---
+++ cpp/tricks/benaphores.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <windows.h>
+#include <intrin.h>
+
+// https://preshing.com/20120226/roll-your-own-lightweight-mutex/
+class Benaphore
+{
+private:
+    LONG m_counter;
+    HANDLE m_semaphore;
+
+public:
+    Benaphore()
+    {
+        m_counter = 0;
+        m_semaphore = CreateSemaphore(NULL, 0, 1, NULL);
+    }
+
+    ~Benaphore()
+    {
+        CloseHandle(m_semaphore);
+    }
+
+    void Lock()
+    {
+        if (_InterlockedIncrement(&m_counter) > 1) // x86/64 guarantees acquire semantics
+        {
+            WaitForSingleObject(m_semaphore, INFINITE);
+        }
+    }
+
+    void Unlock()
+    {
+        if (_InterlockedDecrement(&m_counter) > 0) // x86/64 guarantees release semantics
+        {
+            ReleaseSemaphore(m_semaphore, 1, NULL);
+        }
+    }
+};
